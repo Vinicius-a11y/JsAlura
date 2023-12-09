5d8d4a78f725bf2742bdb5d710a254e16bdea14a
Vinicius Bueno <79877937+Vinicius-a11y@users.noreply.github.com>
2023-12-09 16:42:05 -0300

    strings

--- assets/Index.js
+++ assets/Index.js
@@ -23,3 +23,31 @@ console.log(novaOperacao)
 
 const alura = "Alura";
 console.log(alura * primeiroNumero)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+// const cidade = "belo horizonte";
+// const input = "Belo Horizonte";
+
+// console.log(cidade === input); // false
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
+
+
