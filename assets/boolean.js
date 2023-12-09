a517a544096d1c20c0480d043a4760cb09bf2511
Vinicius Bueno <79877937+Vinicius-a11y@users.noreply.github.com>
2023-12-09 17:29:18 -0300

    boolean

---
+++ assets/boolean.js
@@ -0,0 +1,27 @@
+//boolean 
+//true -> verdadeiro
+//false -> falso
+
+//true
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+//false
+// const primeiroNumero = 5;
+// const segundoNumero = 10;
+
+// console.log(primeiroNumero === segundoNumero);~
+
+
+//false
+const texto1 = "Alura";
+const texto2 = "A"
+
+console.log(texto1 === texto2);
+
+
+
+
