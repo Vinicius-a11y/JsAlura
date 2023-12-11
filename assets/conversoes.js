ec9d2f805d08e493e76536508058b41fedd6fc1f
Vinicius Bueno <BuenoVini455@hotmail.com>
2023-12-11 14:13:28 -0300

    conversão

---
+++ assets/conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+const numero = 456;
+const numeroString = Number("456a");
+
+//Number()
+//Sttring()
+console.log(numero + Number(numeroString));
+
+//conversão explicia
