60bdabe316eecc96d0fc7ccf1bc91968f6508af4
Vinicius Bueno <BuenoVini455@hotmail.com>
2024-01-03 12:10:58 -0300

    up

---
+++ assets/template.string.js
@@ -0,0 +1,9 @@
+const nome = "Vinicius";
+const idade = 2024 - 2001;
+const cidadeDeNascimento = "Parana";
+
+// const apresentacao = "Meu nome é " + nome;
+
+const apresentacao = `meu nome é $(nome), minha idade é $(idade) e nasci na cidade de $(cidadeDeNascimento)`;
+
+console.log(apresentacao);
