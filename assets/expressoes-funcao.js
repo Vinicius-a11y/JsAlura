9c85836592cdcb561dd1c384ee212dc003932944
Vinicius Bueno <BuenoVini455@hotmail.com>
2024-01-08 12:03:36 -0300

    Fim curso up

--- assets/expressoes-funcao.js
+++ assets/expressoes-funcao.js
@@ -1,10 +1,21 @@
-function minhaFuncao(param) {
-	//bloco de Codigo
-}
-minhaFuncao("param");
+// function minhaFuncao(param) {
+//bloco de Codigo
+// }
+// minhaFuncao("param");
 // expressão de função
 
 const soma = function (num1, num2) {
 	return num1 + num2;
 };
+// console.log(soma(1, 1));
+
+//diferença principal ????
+//funções e var são "listadas" no topo do arquivo
+
+console.log(apresenar());
+
+function apresenar() {
+	return "ola";
+}
+
 console.log(soma(1, 1));
